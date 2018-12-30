52bc0615d99b7d5436e66e7856bc5a6fa2ffddcb
cktang88 <arctangent20@yahoo.com>
2018-12-30 10:56:31 -0600

    Init crafty

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
-import canvasDraw from "./sight/render";
+import Crafty from 'craftyjs'
 
-import Game from './game'
+Crafty.init(500, 350, document.getElementById('game'));
 
-window.requestAnimationFrame = window.requestAnimationFrame ||
-	window.webkitRequestAnimationFrame ||
-	window.mozRequestAnimationFrame ||
-	window.msRequestAnimationFrame;
+/*
+Crafty.e('2D, DOM, Color, Fourway')
+    .attr({
+        x: 0,
+        y: 0,
+        w: 100,
+        h: 100
+    })
+    .color('#F00')
+    .fourway(200);*/
 
-function drawLoop() {
-	requestAnimationFrame(drawLoop);
-	/*
-    if(updateCanvas){
-    	canvasDraw();
-    	updateCanvas = false;
-	}*/
-	canvasDraw();
-}
-// main game loop
-window.onload = function () {
-	drawLoop();
-	// TODO: game update logic loop
-	const root = document.getElementById('root');
-	new Game(root);
-};
+Crafty.e('Floor, 2D, Canvas, Color')
+    .attr({
+        x: 0,
+        y: 250,
+        w: 250,
+        h: 10
+    })
+    .color('green');
+
+Crafty.e('2D, Canvas, Color, Twoway, Gravity')
+    .attr({
+        x: 0,
+        y: 0,
+        w: 50,
+        h: 50
+    })
+    .color('#F00')
+    .twoway(200)
+    .gravity('Floor');
+
+
+// selection/events is like d3.js/jQuery http://craftyjs.com/documentation/entities.html
