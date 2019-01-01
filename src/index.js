8b4e8a889d6c2b500967f7a030ee61dd88f44b92
cktang88 <arctangent20@yahoo.com>
2019-01-01 17:11:59 -0600

    mess around

--- src/index.js
+++ src/index.js
@@ -1,18 +1,78 @@
-import Crafty from 'craftyjs'
+import Crafty, {
+    math
+} from "craftyjs";
 
-Crafty.init(500, 350, document.getElementById('game'));
+const {
+    Vector2D
+} = math;
+/**
+ * Crafty.e() = entity
+ * Crafty.c() = component
+ * Crafty.s() = system
+ *
+ */
 
-/*
-Crafty.e('2D, DOM, Color, Fourway')
+Crafty.init(1200, 600, document.getElementById("game"));
+
+const SIZE = 20;
+
+const projectiles = [];
+
+const createBullet = ({
+    x,
+    y,
+    w,
+    h
+}) => {
+    return Crafty.e('2D, DOM, Motion, Collision')
+        .setName('bullet')
+        .attr({
+            x,
+            y,
+            w,
+            h
+        })
+        .color('333')
+
+    // use .ccdbr() for tunneling protection
+    // see https://github.com/craftyjs/Crafty/wiki/Crafty-FAQ-%28draft%29#why-are-my-bullets-passing-through-other-entities-without-registering-hits
+}
+
+// If you want more fine-grained control consider using Crafty.map.search().
+
+const player = Crafty.e("2D, DOM, Color, Mouse, Fourway, Collision, Solid")
+    .setName('player') // just human readable label
     .attr({
         x: 0,
         y: 0,
-        w: 100,
-        h: 100
+        w: SIZE,
+        h: SIZE
     })
-    .color('#F00')
-    .fourway(200);*/
+    .bind("MouseUp", function (e) {
+        // lol, works only when you click on the obj, so global mouse often better
+    })
+    .color("#aaa")
+    .fourway(200);
 
+Crafty.s("Mouse").bind("MouseDown", function (e) {
+    const {
+        realX,
+        realY,
+        target,
+        mouseButton
+    } = e;
+    Crafty.log("Mouse pos: <" + realX.toFixed(2) + ", " + realY.toFixed(2) + ">");
+    console.log(target);
+
+    const start = new Vector2D(realX, realY);
+    const end = new Vector2D(player.x, player.y);
+    console.log(start.subtract(end));
+    const vec = start.subtract(end).getNormal();
+
+
+    if (mouseButton == Crafty.mouseButtons.LEFT) {} else {}
+});
+/*
 Crafty.e('Floor, 2D, Canvas, Color')
     .attr({
         x: 0,
@@ -31,7 +91,6 @@ Crafty.e('2D, Canvas, Color, Twoway, Gravity')
     })
     .color('#F00')
     .twoway(200)
-    .gravity('Floor');
-
+    .gravity('Floor');*/
 
 // selection/events is like d3.js/jQuery http://craftyjs.com/documentation/entities.html
